c2971a11812a2fb8ea6a16c95655552690d5275e
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2021-01-04 23:34:38 -0300

    add label test para termo.texto

--- backend/termos/tests/test_termo_model.py
+++ backend/termos/tests/test_termo_model.py
@@ -14,3 +14,8 @@ class TermoModelTestCase(TestCase):
         termo = Termo.objects.get(id=1)
         lingua_label = termo._meta.get_field('lingua').verbose_name
         self.assertEquals(lingua_label, 'lingua')
+
+    def test_texto_label(self):
+        termo = Termo.objects.get(id=1)
+        texto_label = termo._meta.get_field('texto').verbose_name
+        self.assertEquals(texto_label, 'texto')
