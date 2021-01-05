f261b8586b96e8e18e62d6a312e942920d46cde8
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2021-01-04 23:37:55 -0300

    pass str test para termo

--- backend/termos/models.py
+++ backend/termos/models.py
@@ -5,3 +5,6 @@ from linguas.models import Lingua
 class Termo(models.Model):
     lingua = models.ForeignKey(Lingua, on_delete=models.CASCADE)
     texto = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.texto} ({self.lingua})'
