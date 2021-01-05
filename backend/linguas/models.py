7a43b1b43f930a552f4f4721c1c9c1ff8a03751b
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2021-01-04 23:16:47 -0300

    pass str test

--- backend/linguas/models.py
+++ backend/linguas/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Lingua(models.Model):
     codigo = models.SlugField(primary_key=True)
     descricao = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.codigo
