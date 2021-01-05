a0066d2a123e721aba0e3829389605be66ff601c
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2021-01-04 23:32:47 -0300

    corrige erros no teste

--- backend/termos/tests/test_termo_model.py
+++ backend/termos/tests/test_termo_model.py
@@ -7,9 +7,9 @@ class TermoModelTestCase(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        lingua, created = Lingua.objects.create(codigo='codigo', descricao='descricao teste')
+        lingua = Lingua.objects.create(codigo='codigo', descricao='descricao teste')
         Termo.objects.create(lingua=lingua, texto='texto test')
 
     def test_lingua_label(self):
-        termo = Termo.object.get(id=1)
+        termo = Termo.objects.get(id=1)
         lingua_label = termo._meta.get_field('lingua').verbose_name
